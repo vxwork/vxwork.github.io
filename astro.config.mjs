de6ab26f2e56db34a6d5b52897f989230d4043f9
zhuhaitao12 <zhuhaitao12@jd.com>
2026-04-01 16:17:19 +0800

    edit astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vxwork.github.io',
-    base: '/github-pages'
+	site: 'https://vxwork.github.io',
+	integrations: [mdx(), sitemap()],
 });
